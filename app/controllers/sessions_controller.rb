b0c51fc18c0bda7e25e586456ee9c2a8c335c2e4
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-05-16 00:39:03 +0000

    automatically backed up by learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController 
+  before_action :require_login 
+
+
+  private 
+
+  def require_login 
+    return head(:forbidden) unless session.include? :user_id 
+  end 
+end 
