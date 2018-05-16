c72af8fe989768c6dad96b6f2eae1ea79f5f324a
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-05-16 00:43:55 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,15 @@
 class SessionsController < ApplicationController
-  before_action :require_login
+  def new
+  end
 
+  def create
+    return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
+    session[:name] = params[:name]
+    redirect_to controller: 'application', action: 'hello'
+  end
 
-  private
-
-  def require_login
-    return head(:forbidden) unless session.include? :user_id
-  end 
+  def destroy
+    session.delete :name
+    redirect_to controller: 'application', action: 'hello'
+  end
 end
